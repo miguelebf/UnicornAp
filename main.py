9bf86a60e588876d878d3c710e1cf4e1b73f3689
root <root@localhost.localdomain>
2017-12-02 14:25:03 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+#UnicornAp 1.0
+print("\033[0;31m"+
+"""
+888     888          d8b                                          d8888          
+888     888          Y8P                                         d88888          
+888     888                                                     d88P888          
+888     888 88888b.  888  .d8888b .d88b.  888d888 88888b.      d88P 888 88888b.  
+888     888 888 "88b 888 d88P"   d88""88b 888P"   888 "88b    d88P  888 888 "88b 
+888     888 888  888 888 888     888  888 888     888  888   d88P   888 888  888 
+Y88b. .d88P 888  888 888 Y88b.   Y88..88P 888     888  888  d8888888888 888 d88P 
+ "Y88888P"  888  888 888  "Y8888P "Y88P"  888     888  888 d88P     888 88888P"  
+                                                                        888
+                                                                        888    
+                                                                        888
+"""+'\033[0;m'+
+"""By MByte 
+Version 0.1""")
+
+#Preguntas
+interfaceAp=input("Ingrese nombre de la interface inalambrica: ")
+interfaceInternet=input("Ingrese nombre de la interface conectada a internet: ")
+nombreRedWifi=input("Nombre de la red que desea crear: ")
+seguridad_d=input("Desea que la red tenga password(y/n):  ")
+if seguridad_d=="y" or seguridad_d=="Y" :
+    passwordRed=input("Ingrese un password para el punto de Acceso: ")
+sslstrip_d=input("Desea activar Sslstrip+: ")
+dnsSpoofing_d=input("Desea activar DnsSpoofing: ")
+
+
+
+
+
+
+
+
+
+
+
